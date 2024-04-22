16e5e17106de4818c8eabb974447ddc89e50f47b
Can <bayramcan1515@gmail.com>
2024-04-22 21:09:37 +0300

    finding the largest number in functions

---
+++ c/largestnumberinfunctions.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//this code written by Can Çetin
+//This c code defines a function that finds the largest number in an array and prints it out.
+//This code also defines a macro for pi.
+
+
+#define yazdır printf
+#define oku scanf
+#define pi 3.14
+
+
+void largestnumber(int array[], int size){
+    int largestvalue = 0;
+    for(int i = 0; i < size; i++){
+        if(array[i] > largestvalue){
+            largestvalue = array[i];
+        }
+    }
+    printf("Largest number in the array is: %d\n", largestvalue);
+}
+
+
+
+int main(){
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(array) / sizeof(array[0]);
+    largestnumber(array, size);
+
+    yazdır("pi: %f\n", pi);
+
+    return 0;
+}
